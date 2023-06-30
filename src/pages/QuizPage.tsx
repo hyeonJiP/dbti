88c1bf7de2684f446c351aeef9794bc521bae3ed
hyeonJiP <altk5700@gmail.com>
2023-06-30 23:54:58 +0900

    quize

--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -1,17 +1,33 @@
 import { FC, useState } from "react";
 import { ShowProps } from "../interface/interface";
 import { questions } from "./contents/questions";
+import { styled } from "styled-components";
+
+const Wrapper = styled.div<ShowProps>`
+  display: ${(props) => (props.isShow === true ? "flex" : "none")};
+  width: 100%;
+  height: 100vh;
+  background-color: white;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 const QuizPage: FC<ShowProps> = ({ isShow }) => {
   const [questionIdx, setQuestionIdx] = useState(0);
+  const [answer, setAnswer] = useState<string[]>([]); // 결과 페이지
+  const [isQuizFinished, setIsQuizFinished] = useState(false);
+  const [isCalculatingResult, setCalculatingResult] = useState(false); // 계산중 페이지
 
   const test = questions[0].question;
 
   return (
     <>
-      {questions[0].question.split("<br/>").map((line, index) => (
-        <p key={index}>{line}</p>
-      ))}
+      <Wrapper isShow={isShow}>
+        {questions[0].question.split("<br/>").map((word, index) => (
+          <p key={index}>{word}</p>
+        ))}
+      </Wrapper>
     </>
   );
 };
---
+++ src/pages/calculateMBTI.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react";
+
+const calculateMBTI: FC = () => {
+  // 인덱스 시그니처 <key,value>
+  // type mbti = {
+  //   [key:string]:number
+  // }
+  const mbti: { [key: string]: number } = {
+    E: 0,
+    I: 0,
+    S: 0,
+    N: 0,
+    T: 0,
+    F: 0,
+    J: 0,
+    P: 0,
+  };
+  return <></>;
+};
+export default calculateMBTI;
