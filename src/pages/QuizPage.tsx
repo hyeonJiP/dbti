4628b8b08d8869dcba09c5e48e6bcac989541615
hyeonJiP <altk5700@gmail.com>
2023-07-01 16:33:10 +0900

    question

--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -13,20 +13,44 @@ const Wrapper = styled.div<ShowProps>`
   justify-content: center;
 `;
 
-const QuizPage: FC<ShowProps> = ({ isShow }) => {
-  const [questionIdx, setQuestionIdx] = useState(0);
-  const [answer, setAnswer] = useState<string[]>([]); // 결과 페이지
-  const [isQuizFinished, setIsQuizFinished] = useState(false);
-  const [isCalculatingResult, setCalculatingResult] = useState(false); // 계산중 페이지
+const Container = styled.div`
+  margin-bottom: 3rem;
+  text-align: center;
+  align-items: center;
+`;
 
-  const test = questions[0].question;
+const Text = styled.div`
+  font-family: "Jalnan";
+  font-size: 1.9rem;
+  font-weight: light;
+  margin: 0.5rem;
+  text-align: center;
+  color: black;
+  margin-top: 3.9rem;
+  margin-bottom: 8.4rem;
+  line-height: 50px;
+`;
+
+const QuizPage: FC<ShowProps> = ({ isShow }) => {
+  const [questionIdx, setQuestionIdx] = useState(0); // 질문 count
+  const [answer, setAnswer] = useState<string[]>([]); // 선택된 문항 배열
+  const [isQuizFinished, setIsQuizFinished] = useState(false); // 결과 페이지
+  const [isCalculatingResult, setIsCalculatingResult] = useState(false); // 계산중 페이지
+
+  const currentQuestion = questions[questionIdx]; // 질문 문항
 
   return (
     <>
       <Wrapper isShow={isShow}>
-        {questions[0].question.split("<br/>").map((word, index) => (
-          <p key={index}>{word}</p>
-        ))}
+        <Container>
+          <Text>
+            {currentQuestion.question.split("<br/>").map((word, index) => (
+              <p key={index}>{word}</p>
+            ))}
+          </Text>
+        </Container>
+
+        <button>버튼</button>
       </Wrapper>
     </>
   );
